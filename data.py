68b5246e61d10741ef314405f6cb18334047e250
Richard Guinto <richard.guinto@gmail.com>
2018-06-13 06:04:50 +0800

    Updated data loader.

--- data.py
+++ data.py
@@ -10,12 +10,40 @@ import numpy as np
 import os.path
 import glob
 
-default_path = '/datasets/CELABA/img_celeba'
+#default_path = '/datasets/CELABA/img_celeba'
+default_path = '../MSCOCO/test2017'
+output_dir = 'output'
+np.random.seed(3)
 
-def loader(batch_size = 64, path = default_path):
+def loader(batch_size = 8, path = default_path):
+    images = []
+    counter = 0
     while True:
         files = glob.glob(os.path.join(path, '*.jpg'))
         for file in files:
             img = cv2.imread(file)
-            yield img
-            
+            ksize = np.random.randint(5,20)
+            # generating the kernel
+            kernel_motion_blur = np.zeros((ksize, ksize))
+            kernel_motion_blur[int((ksize-1)/2), :] = np.ones(ksize)
+            kernel_motion_blur = kernel_motion_blur / ksize
+            # applying the kernel to the input image
+            output = cv2.filter2D(img, -1, kernel_motion_blur)
+            print('file: %s ksize %d' % (file, ksize))
+            images.append(output)
+            counter = counter + 1
+            output_name = output_dir + '/output' + str(counter) + '_' + str(ksize) + '.jpg'
+            cv2.imwrite(output_name, output)
+            if len(images) == batch_size:
+                yield images
+                images.clear()
+
+def main():
+    generator = loader()
+    imgs = next(generator)
+    print(len(imgs))
+    imgs = next(generator)
+    
+
+if __name__ == '__main__':
+    main()
